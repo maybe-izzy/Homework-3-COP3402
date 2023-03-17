2da63f4f0b8941d4b1c5fb27bd771b6338f461cc
maybe-izzy <123502522+maybe-izzy@users.noreply.github.com>
2023-03-17 08:14:31 -0400

    integrate our lexer

--- lexer.c
+++ lexer.c
@@ -1,414 +1,347 @@
-/* $Id: lexer.c,v 1.10 2023/02/24 17:12:16 leavens Exp leavens $ */
+/*=============================================================================
+| Assignment: HW 2 - Lexer 
+| Authors: Isabelle Montgomery and Christopher Colon Marquez
+| Language: C
+| Class: COP3402 (0001) - Systems Software - Spring 2023
+| Instructor: Gary T. Leavens
+| Due Date: 02/14/2023
+|
+| This program implements a lexical analyzer in C. 
++=============================================================================*/
+
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
-#include <ctype.h>
+#include <string.h>
 #include <limits.h>
+#include <ctype.h>
 #include "token.h"
+#include "lexer_output.h"
 #include "utilities.h"
-#include "lexer.h"
-#include "reserved.h"
 
-// The input file
-static FILE *input_file = NULL;
-// The input file's name
-static const char *filename = NULL;
-// Is this token stream done (past EOF or error)?
-static bool done = true;
-// the line of the next token
-static unsigned int line;
-// the column of the next token
-static unsigned int column;
+// Variables associated with lexer 
+int done_flag; 
+unsigned int line; 
+unsigned int column; 
+FILE *file_ptr = NULL; 
+const char *file_name = NULL; 
+char buffer[MAX_IDENT_LENGTH + 1]; 
+char legal_symbols[] = {'>', '<', '(', ')', '*', '+', '-', '/', ':', ';', ',', '.', '='}; 
 
-// Check the lexer's invariant
-static void lexer_okay()
-{
-    assert(done == (input_file == NULL));
-    assert(done == (filename == NULL));
-}
-
-// Initialize the lexer (i.e., its data structures)
-static void lexer_initialize()
-{
-    filename = NULL;
-    input_file = NULL;
-    done = true;
-    line = 1;
-    column = 1;
-    reserved_initialize();
-}
-
-// Requires: fname != NULL
-// Requires: fname is the name of a readable file
-// Initialize the lexer and start it reading
-// from the given file name
-void lexer_open(const char *fname)
-{
-    lexer_initialize();
-    input_file = fopen(fname, "r");
-    if (input_file == NULL) {
-	bail_with_error("Cannot open %s", fname);
+// Returns token type for a string input character 
+int string_type(){
+    if (strcmp(buffer, "var") == 0){
+        return varsym; 
     }
-    filename = fname;
-    done = feof(input_file);
-    if (done) {
-	input_file = NULL;
-	filename = NULL;
+    else if (strcmp(buffer, "do") == 0){
+        return dosym; 
     }
-    lexer_okay();
+    else if (strcmp(buffer, "skip") == 0){
+        return skipsym; 
+    }
+    else if (strcmp(buffer, "const") == 0){
+        return constsym; 
+    }
+    else if (strcmp(buffer, "begin") == 0){
+        return beginsym; 
+    }
+    else if (strcmp(buffer, "call") == 0){
+        return callsym; 
+    }
+    else if (strcmp(buffer, "procedure") == 0){
+        return procsym; 
+    }
+    else if (strcmp(buffer, "end") == 0){
+        return endsym; 
+    }
+    else if (strcmp(buffer, "write") == 0){
+        return writesym; 
+    }
+    else if (strcmp(buffer, "read") == 0){
+        return readsym; 
+    }
+    else if (strcmp(buffer, "if") == 0){
+        return ifsym; 
+    }
+    else if (strcmp(buffer, "then") == 0){
+        return thensym; 
+    }
+    else if (strcmp(buffer, "else") == 0){
+        return elsesym; 
+    }
+    else if (strcmp(buffer, "while") == 0){
+        return whilesym; 
+    }
+    else if (strcmp(buffer, "odd") == 0){
+        return oddsym; 
+    }
+    else {
+        return identsym; 
+    }
 }
 
-// Close the file the lexer is working on
-// and make this lexer be done
-void lexer_close()
-{
-    lexer_okay();
-    if (input_file != NULL) {
-	int rc = fclose(input_file);
-	if (rc == EOF) {
-	    bail_with_error("Cannot close %s!", filename);
-	}
+// Checks if a character is legal in the lexer's accepted alphabet 
+void is_legal(char c){
+    if (isalpha(c) || isdigit(c) || isspace(c) || (c == EOF)){
+        return; 
     }
-    input_file = NULL;
-    filename = NULL;
-    done = true;
-    lexer_okay();
+    // If character is symbol, verify 
+    else {
+        for (int i = 0; i < 13; i++){
+            if (c == legal_symbols[i]){
+                return; 
+            }
+        }
+        char error[50]; 
+        sprintf(error, "Illegal character '%c' (%.3o)", c, c); 
+        lexical_error(file_name, lexer_line(), lexer_column(), error);
+    }
 }
 
-// Is the lexer's token stream finished
-// (either past EOF or not open)?
-bool lexer_done()
-{
-    return done;
+// "Resets" the buffer for character intake
+void buffer_reset(){
+    strcpy(buffer, ""); 
 }
 
-// the previous value of column,
-// for use in lexer_ungetchar
-static unsigned int last_column = 0;
-
-// Requires: input_file is readable
-// Return the next char in the input
-// updating line and column as appropriate
-// update last_column to the old value of column
-static char lexer_getchar()
-{
-    char c = getc(input_file);
-    last_column = column;
-    if (c == '\n') {
-	line++;
-	column=1;
-    } else {
-	column++;
+void lexer_open(const char *fname){
+    FILE *fp = fopen(fname, "r"); 
+    
+    // Make sure that the program was passed a valid filename 
+    if (fp == NULL){
+        bail_with_error("Invalid file name");
     }
+
+    // Initialize the lexer
+    column = 0; 
+    line = 1; 
+    file_name = fname; 
+    file_ptr = fp; 
+    done_flag = 0; 
+    buffer_reset(); 
+}
+
+void lexer_close(){
+    fclose(file_ptr); 
+}
+
+bool lexer_done(){
+    if (done_flag){
+        return true; 
+    }
+    return false; 
+}
+
+const char *lexer_filename(){
+    return file_name; 
+}
+
+unsigned int lexer_line(){
+    return line; 
+}
+
+unsigned int lexer_column(){
+    int len = strlen(buffer); 
+    if (len > 0){
+        return (column -  strlen(buffer) + 1);
+    }
+    else {
+        return column; 
+    }
+}
+
+// Push character to the buffer 
+void buffer_cat(char c){
+    strncat(buffer, &c, 1); 
+}
+
+// Function to assemble a token 
+token assemble_token(token_type type){
+    token new_token; 
+
+    new_token.typ = type; 
+    new_token.column = lexer_column(); 
+    new_token.line = lexer_line(); 
+
+    if (type == numbersym){
+        new_token.value = atoi(buffer); // Already have error handling for vals > length of short
+    }
+    if (type == eofsym){
+        new_token.text = NULL; 
+    }
+    else {
+        new_token.text = (char*) malloc(sizeof(buffer + 1) * sizeof(char)); 
+        strcpy(new_token.text, buffer); 
+    }
+    new_token.filename = file_name; 
+    buffer_reset(); 
+    return new_token; 
+}
+
+// Gets a character from input and appends it to the running buffer 
+char get_character(){
+    column++;
+    char c = getc(file_ptr);  
+    buffer_cat(c);
+
     return c;
 }
 
-// Requires: input_file is readable
-// Put c back into the input file stream
-// to be read again
-static void lexer_ungetchar(char c)
-{
-    column = last_column;
-    if (c == '\n') {
-	line--;
-    }
-    if (c != EOF) {
-	ungetc(c, input_file);
-    }
+// Pushes a character back to input 
+void put_back(){
+    ungetc(buffer[strlen(buffer) - 1], file_ptr); 
+    buffer[strlen(buffer) - 1] = '\0'; 
+    column--; 
 }
 
-// forward declarations of lexical functions
-static void lexer_consume_ignored();
-static token lexer_ident(char c, token t);
-static token lexer_number(char c, token t);
-static token lexer_becomes(char c, token t);
-static token lexer_starts_less(char c, token t);
-static token lexer_starts_greater(char c, token t);
+// Eats whitespace/comments until meaningful input is encountered
+void eat_characters(){ 
+    int stop_eating = 0; 
+    char current_char; 
 
-// Requires: !lexer_done()
-// Return the next token in the input file,
-// advancing in the input
-token lexer_next()
-{
-    token t;
-    t.filename = filename;
-    t.typ = eofsym;
-    t.text = NULL;
-    t.value = 0;
+    while (!stop_eating){
+        current_char =  get_character(); 
+        
+        // Handle whitespace
+        if (isspace(current_char)){
+            if (current_char == '\n'){
+                line++; 
+                column = 0;  
+            }
+        }
+        // Handle comments 
+        else if (current_char == '#'){  
+            while (current_char != '\n'){
+                current_char = get_character(); 
+                if (current_char == EOF){
+                    lexical_error(file_name, lexer_line(), column, "File ended while reading comment!");
+                }
+                // Remove comment character from the buffer 
+                buffer_reset(); 
+            }
+            ungetc(current_char, file_ptr); 
+        }
+        else {
+            stop_eating = 1; 
+        }
+    }   
+    // Put back the meaningful input which was encountered and reset the buffer
+    put_back();
+    buffer_reset();  
+}
 
-    lexer_consume_ignored();
+token lexer_next(){
+    eat_characters(); 
+    char error[50]; 
 
-    t.line = line;
-    t.column = column;
+    char current_char = get_character(); 
+    is_legal(current_char); 
+    char next_char; 
 
-    char c = lexer_getchar(input_file);
+    // Detect end of input 
+    if (current_char == EOF){
+        done_flag = 1; 
+        return assemble_token(eofsym); 
+    }
+    // Detect keywords and identifiers  
+    else if (isalpha(current_char)){ 
+        next_char = get_character(); 
+
+        while (isalpha(next_char) || isdigit(next_char)){
+            next_char = get_character(); 
+            
+            // Ensure input is not running beyond the max acceptable length 
+            if (strlen(buffer) >= MAX_IDENT_LENGTH){
+                lexical_error(file_name, lexer_line(), lexer_column(), "Identifier starting \"%s\" is too long!", buffer);
+            }
+        }
+        put_back(); 
+        // Assemble a token with an appropriate type for the string input 
+        return assemble_token(string_type()); 
+    }
+    // Detect numerical input  
+    else if (isdigit(current_char)){
+        next_char = get_character(); 
     
-    // since we consumed all the whitespace
-    // c should not be a kind of space character
-    assert(!isspace(c));
-    
-    if (c == EOF) {
-	t.typ = eofsym;
-	t.text = NULL;
-	filename = NULL;
-	input_file = NULL;
-	done = true;
-	return t;
+        while (isdigit(next_char)){
+            // Ensure input is not running beyond the max acceptable length 
+            if ((atoi(buffer) > SHRT_MAX) || (atoi(buffer) < SHRT_MIN)){
+                sprintf(error, "The value of %d is too large for a short!", atoi(buffer)); 
+                lexical_error(file_name, lexer_line(), lexer_column(), error);
+            }
+            next_char = get_character();
+        }
+        put_back(); 
+        return assemble_token(numbersym); 
     }
-    if (isalpha(c)) {
-	return lexer_ident(c, t);
-    } else if (isdigit(c)) {
-	return lexer_number(c, t);
-    } else {
-	// leave room in t.text for 2 chars of text
-	t.text = malloc(3 * sizeof(char));
-	if (t.text == NULL) {
-	    bail_with_error("Cannot allocate space for token text!");
-	}
-	t.text[0] = c;
-	t.text[1] = '\0';
-	switch (c) {
-	case '.':
-	    t.typ = periodsym;
-	    break;
-	case ';':
-	    t.typ = semisym;
-	    break;
-	case ',':
-	    t.typ = commasym;
-	    break;
-	case ':':
-	    return lexer_becomes(c, t);
-	case '=':
-	    t.typ = eqsym;
-	    break;
-	case '(':
-	    t.typ = lparensym;
-	    break;
-	case ')':
-	    t.typ = rparensym;
-	    break;
-	case '<':
-	    return lexer_starts_less(c, t);
-	    break;
-	case '>':
-	    return lexer_starts_greater(c, t);
-	    break;
-	case '+':
-	    t.typ = plussym;
-	    break;
-	case '-':
-	    t.typ = minussym;
-	    break;
-	case '*':
-	    t.typ = multsym;
-	    break;
-	case '/':
-	    t.typ = divsym;
-	    break;
-	default:
-	    lexical_error(filename, line, column-1,
-			  "Illegal character '%c' (0%o)",
-			   c, c);
-	    break;
-	}
-	return t;
+    // Detect punctuation input 
+    else {
+        if (current_char == ':'){
+            next_char = get_character(); 
+            if (next_char == '='){
+                return assemble_token(becomessym);  
+            }
+            else { 
+                // Since error is specific to character at current column, use the non-adjusted column value
+                lexical_error(file_name, lexer_line(), column, "Expecting '=' after a colon, not '%c'", next_char);
+                return assemble_token(-1);  
+            }
+        }
+        else if (current_char == ';'){
+            return assemble_token(semisym); 
+        }
+        else if (current_char == '.'){
+            return assemble_token(periodsym); 
+        }
+        else if (current_char == ','){
+            return assemble_token(commasym); 
+        }
+        else if (current_char == '='){
+            return assemble_token(eqsym); 
+        }
+        else if (current_char == '('){
+            return assemble_token(lparensym); 
+        }
+        else if (current_char == ')'){
+            return assemble_token(rparensym); 
+        }
+        else if (current_char == '<'){
+            char next_char = get_character(); 
+            if (next_char == '>'){
+                return assemble_token(neqsym); 
+            }
+            else if (next_char == '='){
+                return assemble_token(leqsym); 
+            }
+            else {
+                put_back(); 
+                return assemble_token(lessym); 
+            }
+        }
+        else if (current_char == '>'){
+            char next_char = get_character(); 
+            if (next_char == '='){
+                return assemble_token(geqsym); 
+            }
+            else {
+                put_back(); 
+                return assemble_token(gtrsym); 
+            }
+        }
+        else if (current_char == '+'){
+            return assemble_token(plussym); 
+        }
+        else if (current_char == '-'){
+            return assemble_token(minussym); 
+        }
+        else if (current_char == '/'){
+            return assemble_token(divsym); 
+        }
+        else if (current_char == '*'){
+            return assemble_token(multsym); 
+        }
+        else {
+            lexical_error(file_name, line, column, "Unidentified character"); 
+            return assemble_token(-1); 
+        }
     }
-}
-
-// Requires: !lexer_done()
-// Return the name of the current file
-const char *lexer_filename()
-{
-    if (lexer_done()) {
-	bail_with_error("Asking for file name of done lexer!");
-    }
-    return filename;
-}
-
-// Requires: !lexer_done()
-// Return the line number of the next token
-unsigned int lexer_line()
-{
-    if (lexer_done()) {
-	bail_with_error("Asking for line of done lexer!");
-    }
-    return line;
-}
-
-// Requires: !lexer_done()
-// Return the column number of the next token
-unsigned int lexer_column()
-{
-    if (lexer_done()) {
-	bail_with_error("Asking for column of done lexer!");
-    }
-    return column;
-}
-
-// Requires: input_file is readable
-// Advance input_file to the next newline
-static void lexer_consume_comment()
-{
-    char c = lexer_getchar();
-    while (c != '\n' && c != EOF) {
-	c = lexer_getchar();
-    }
-    if (c == EOF) {
-	lexical_error(filename, line, column-1,
-		      "File ended while reading comment!");
-    }
-    // assert(c == '\n');
-}
-
-// Requires: input_file is readable
-// Advance in the input file until
-// the next char is the start of a token
-// that is not ignored
-// (i.e., not whitespace or a comment)
-static void lexer_consume_ignored()
-{
-    char c = lexer_getchar();
-    while (isspace(c) || c == '#') {
-	if (isspace(c)) {
-	    // ignore the whitespace char
-	    c = lexer_getchar();
-	} else if (c == '#') {
-	    lexer_consume_comment();
-	    c = lexer_getchar();
-	}
-    }
-    // assert(!isspace(c) && c != '#');
-    lexer_ungetchar(c);
-}
-
-// Requires: c is a letter
-// Return a token for a reserved word
-// or an identifier
-static token lexer_ident(char c, token t)
-{
-    char *text = malloc((MAX_IDENT_LENGTH+1)*sizeof(char));
-    if (text == NULL) {
-	bail_with_error("Cannot allocate space for identifier");
-    }
-    text[0] = c;
-    int n = 1;
-    c = lexer_getchar();
-    while (isalpha(c) || isdigit(c)) {
-	if (n >= MAX_IDENT_LENGTH) {
-	    text[n] = '\0';
-	    lexical_error(filename, t.line, t.column,
-			  "Identifier starting \"%s\" is too long!",
-			  text);
-	}
-	text[n] = c;
-	n++;
-	c = lexer_getchar();
-    }
-    // assert(!isalpha(c) && !isdigit(c));
-    text[n+1] = '\0';    
-    lexer_ungetchar(c);
-    t.text = text;
-    t.typ = reserved_type(text);
-    return t;
-}
-
-#define MAX_NUM_LENGTH 5
-
-// Requires: c is a digit
-// Return a token for a number
-static token lexer_number(char c, token t)
-{
-    char *text = malloc((MAX_NUM_LENGTH+1)*sizeof(char));
-    if (text == NULL) {
-	bail_with_error("Cannot allocate space for number");
-    }
-    text[0] = c;
-    int n = 1;
-    c = lexer_getchar();
-    while (isdigit(c)) {
-	if (n >= MAX_NUM_LENGTH) {
-	    text[n] = '\0';
-	    bail_with_error(filename, t.line, t.column,
-			    "Number starting \"%s\" is too long!",
-			    text);
-	}
-	text[n] = c;
-	n++;
-	c = lexer_getchar();
-    }
-    text[n+1] = '\0';
-    lexer_ungetchar(c);
-    t.text = text;
-    int val;
-    sscanf(text, "%d", &val);
-    if (val > SHRT_MAX) {
-	lexical_error(filename, t.line, t.column,
-		      "The value of %s is too large for a short!",
-		      text);
-    }
-    t.value = val;
-    t.typ = numbersym;
-    return t;
-}
-
-// Requires: c is a colon character (:)
-// Returns the token for a becomessym
-static token lexer_becomes(char c, token t)
-{
-    assert(c == ':');
-    c = lexer_getchar();
-    if (c != '=') {
-	lexical_error(filename, line, column-1,
-		      "Expecting '=' after a colon, not '%c'",
-		      c);
-    }
-    t.text[1] = c;
-    t.text[2] = '\0';
-    t.typ = becomessym;
-    return t;
-}
-
-// Requires: c is a less-than character (<)
-// Returns the token appropriate for the next char
-static token lexer_starts_less(char c, token t)
-{
-    assert(c == '<');
-    c = lexer_getchar();
-    t.text[1] = c;
-    t.text[2] = '\0';
-    switch (c) {
-    case '=':
-	t.typ = leqsym;
-	break;
-    case '>':
-	t.typ = neqsym;
-	break;
-    default:
-	t.text[1] = '\0';
-	lexer_ungetchar(c);
-	t.typ = lessym;
-	break;
-    }
-    return t;
-}
-
-static token lexer_starts_greater(char c, token t)
-{
-    assert(c == '>');
-    c = lexer_getchar();
-    t.text[1] = c;
-    t.text[2] = '\0';
-    switch (c) {
-    case '=':
-	t.typ = geqsym;
-	break;
-    default:
-	t.text[1] = '\0';
-	lexer_ungetchar(c);
-	t.typ = gtrsym;
-	break;
-    }
-    return t;
-
 }
